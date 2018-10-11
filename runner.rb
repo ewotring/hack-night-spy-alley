811c75fa61bdb31e1c21cf3d695d32feeeb9e766
Erik Wotring <ewotring@gmail.com>
2018-10-10 20:11:43 -0500

    Build spy objects from user input

--- runner.rb
+++ runner.rb
@@ -1,6 +1,7 @@
 require 'json'
 require_relative 'board_printer'
 require_relative 'score_card_printer'
+require './spy'
 
 board_spaces = JSON.load(File.new(__dir__ + '/data/board_spaces.json'), symbolize_names: true)
 
@@ -13,3 +14,33 @@ score_card_printer = ScoreCardPrinter.new(identities, spy_items)
 puts board_printer.to_s
 puts
 puts score_card_printer.to_s
+
+# Start by gathering the players and their identities
+# players = []
+new_identities = identities
+# spy_ids = Hash.new
+score_card = [false, false, false, false]
+spy_objects = Hash.new
+until new_identities.length == 0
+  puts "Enter Player Name"
+  input = gets.chomp
+  puts "the new player is #{input}"
+  rando = new_identities.sample
+  puts "Your identity is #{rando}"
+  new_identities.delete(rando)
+  puts "remaining ids are #{new_identities}"
+  spy_single = Spy.new(rando, score_card)
+  # spy_ids[input] = rando
+  # puts "Spies are #{spy_ids}"
+  spy_objects[input] = spy_single
+  puts "spy_objects is #{spy_objects}"
+end
+
+starting_bank = 10 * spy_objects.length
+puts "starting_bank is #{starting_bank}"
+
+spy_objects.each_value { |spy|
+  p spy
+  spy.bank = starting_bank
+  puts "Bank is #{spy.bank}"
+}
--- spy.rb
+++ spy.rb
@@ -1,8 +1,33 @@
 class Spy
+attr_accessor :bank
 
-  def initialize(identity, score_card)
+  def initialize(identity, score_card, bank = 0)
     @identity = identity
     @score_card = score_card
+    @bank = bank
+    # make score_card a hash or array with booleans
+  end
+
+  def choose_identity(identity)
+
+  end
+
+  def collect_money
+  end
+
+  def pay_money
+  end
+
+  def purchase_spy_item
+  end
+
+  def receive_move_card
+  end
+
+  def receive_free_gift
+  end
+
+  def make_another_move
   end
 
 end
